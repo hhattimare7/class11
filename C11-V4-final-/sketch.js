3e3fa88b98cd66e3482a795df82639cc15dd1e7e
hhattimare7 <77083697+hhattimare7@users.noreply.github.com>
2022-07-30 14:25:57 +0530

    Update sketch.js

--- C11-V4-final-/sketch.js
+++ C11-V4-final-/sketch.js
@@ -1,1 +1,28 @@
 
+var trex, trexAnime;
+
+
+function preload(){
+//load all media file
+trexAnime = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+}
+
+
+function setup(){
+//execute once
+createCanvas(1200,600);
+
+trex = createSprite(200,200);
+trex.addAnimation("trexRunning", trexAnime);
+trex.scale = 0.5;
+}
+
+
+function draw(){
+//execute continuously
+background("black");
+
+drawSprites(); 
+}
+
+
